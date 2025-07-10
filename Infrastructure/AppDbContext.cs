be71a9adeb300dcaa3733998c1a10026d7580fce
Striker1238 <stalker2404@list.ru>
2025-07-10 10:28:21 +0300

    Добавьте файлы проекта.

---
+++ Infrastructure/AppDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Reflection.Emit;
+using System.Xml.Linq;
+
+namespace Infrastructure
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Point> Points { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Point>()
+                .HasMany(p => p.Comments)
+                .WithOne()
+                .HasForeignKey(c => c.PointId);
+        }
+    }
+
+}
